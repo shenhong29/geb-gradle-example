d61c2fd0997274b5e4ad7c67f69ee4fb768f272a
catkinson <catkinson@bloomhealthco.com>
2016-10-11 15:25:55 -0500

    Adding support for running tests in Chrome

--- README.md
+++ README.md
@@ -2,8 +2,14 @@
 
 Example project of running Geb tests with Gradle.
 
-To run the tests, execute:
+To run the tests in Firefox (requires Firefox v47.0.1 installed), execute:
 
 ```
 gradlew test
+```
+
+To run the tests in Chrome, execute:
+
+```
+gradlew test -Dgeb.env=chrome
 ```
--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,4 +1,6 @@
+import io.github.bonigarcia.wdm.ChromeDriverManager
 import org.openqa.selenium.Dimension
+import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 
 // Location where Geb saves the screenshots and HTML dumps at the end of each test
@@ -9,4 +11,22 @@ driver = {
   theDriver.manage().window().setSize(new Dimension(1366, 768))
 
   return theDriver
+}
+
+environments {
+
+  // run with "gradlew -Dgeb.env=chrome testBrowser"
+  // ChromeDriver reference: https://sites.google.com/a/chromium.org/chromedriver/
+  chrome {
+    // Download and configure ChromeDriver using https://github.com/bonigarcia/webdrivermanager
+    ChromeDriverManager.getInstance().setup()
+
+    driver = {
+      def theDriver = new ChromeDriver()
+
+      theDriver.manage().window().setSize(new Dimension(1366, 768))
+
+      return theDriver
+    }
+  }
 }
